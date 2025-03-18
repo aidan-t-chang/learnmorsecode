7671846fc4674634881f6fce4f41d913ad394788
plinygg <aidan.chang@outlook.com>
2025-03-17 23:58:29 -0600

    finished the duration and distance between presses + started functionality of the morse code table

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 // track both time between presses and length of press?
 let count = 0;
-// track the duration
+let first = null;
+let count2 = 0;
+
+// track the duration and the distance
 const button = document.getElementById('press');
 let startTime;
 button.addEventListener('mousedown', (event) => {
     startTime = new Date().getTime();
+    if (first != null) { // the button has been pressed beforehand
+        const timeElapsed = Date.now() - first; // first is the last button lift - the current time = time elapsed between presses
+        if (count2 > 0) {
+            document.getElementById('distanceText').append(document.createTextNode(', '));
+        }
+        const newdist = document.createTextNode(timeElapsed);
+        document.getElementById('distanceText').appendChild(newdist);
+        first = Date.now();
+        count2++;
+    }
+    // if it is the first time the button is being pressed, it doesn't matter
     });
 
 button.addEventListener('mouseup', (event) => {
@@ -16,11 +30,24 @@ button.addEventListener('mouseup', (event) => {
     const newnode = document.createTextNode(duration);
     document.getElementById('durationText').appendChild(newnode);
     count++;
+
+    first = Date.now(); // now that the button is no longer pressed, the time starts
     });
 
 // for mobile users (touchstart, touchend)
 button.addEventListener('touchstart', (event) => {
     startTime = new Date().getTime();
+    if (first != null) { // the button has been pressed beforehand
+        const timeElapsed = Date.now() - first; // first is the last button lift - the current time = time elapsed between presses
+        if (count2 > 0) {
+            document.getElementById('distanceText').append(document.createTextNode(', '));
+        }
+        const newdist = document.createTextNode(timeElapsed);
+        document.getElementById('distanceText').appendChild(newdist);
+        first = Date.now();
+        count2++;
+    }
+    // if it is the first time the button is being pressed, it doesn't matter
     });
 
 button.addEventListener('touchend', (event) => {
@@ -32,29 +59,9 @@ button.addEventListener('touchend', (event) => {
     const newnode = document.createTextNode(duration);
     document.getElementById('durationText').appendChild(newnode);
     count++;
+
+    first = Date.now();
     });
-
-// track the distancne between each press, potentially for tracking spaces
-let first = null;
-let count2 = 0;
-
-button.addEventListener('click', () => {
-    const currentTime = Date.now();
-  
-    if (first === null) {
-        first = currentTime;
-    } 
-    else {
-        const timeElapsed = currentTime - first;
-        if (count2 > 0) {
-            document.getElementById('distanceText').append(document.createTextNode(', '));
-        }
-        const newdist = document.createTextNode(timeElapsed);
-        document.getElementById('distanceText').appendChild(newdist);
-        first = currentTime;
-        count2++;
-    }
-  });
 
 function reset() {
     document.getElementById('durationcontainer').innerHTML = '';
@@ -74,10 +81,23 @@ function reset() {
     first = null;
 }
 
+let count3 = 0;
+function makeVisible() {
+    if (count3 % 2 != 0) {
+        document.getElementById('toggle').style.display = 'none';
+    }
+    else {
+        document.getElementById('toggle').style.display = 'block';
+    }
+    count3++;
+}
+
+document.getElementById('checkbox').addEventListener('click', makeVisible);
 document.getElementById('reset').addEventListener('click', reset);
 
 // TODO:
 // - add togglable morse code table
 // - make it so that you are actually able to input morse code
 // - api generate quotes to practice morse code
-// - figure out the logistics between spaces between long presses vs reg space between long presses
+// - figure out the logistics between spaces between long presses vs reg space between long presses ✅
+// - settings menu to customize the time for dot, dash, and space
